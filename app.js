a52ff884f629329d5f91e0e280e17daf1e95d82c
ledsun <shigeru.nakajima@gmail.com>
2015-06-05 03:27:49 +0900

    Update any files.

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-'use strict';
-
-var connect = require('connect'),
-	connectRoute = require('connect-route'),
-	gitlab = require('./lib/gitlab');
+import connect from 'connect'
+import connectRoute from 'connect-route'
+import gitlab from './lib/gitlab';
 
 connect()
-	.use(connect.json())
-	.use(connectRoute(function(router) {
-		router.post('/gitlab/:chatId', gitlab);
-	}))
-	.listen(process.env.PORT || 3000);
+  .use(connect.json())
+  .use(connectRoute(router => {
+    router.post('/gitlab/:chatId', gitlab);
+  }))
+  .listen(process.env.PORT || 3000);

956acdc6a9135680fa081055d22df4cb62a949c2
ledsun <shigeru.nakajima@gmail.com>
2015-06-08 10:21:16 +0900

    Update the connect.

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import connect from 'connect'
+import bodyParser from 'body-parser'
 import connectRoute from 'connect-route'
 import gitlab from './lib/gitlab';
 
 connect()
-  .use(connect.json())
+  .use(bodyParser.json())
   .use(connectRoute(router => {
     router.post('/gitlab/:chatId', gitlab);
   }))

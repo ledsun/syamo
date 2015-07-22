e0d60eeb25ac611503a320e7b621627790b142d5
ledsun <shigeru.nakajima@gmail.com>
2015-07-22 10:03:02 +0900

    Ignore the update of issues.

--- spec/dev/issue_spec.js
+++ spec/dev/issue_spec.js
@@ -1,22 +1,30 @@
 var frisby = require('frisby');
+var extend = require('xtend');
 
 var issueData = {
-    "object_kind": "issue",
-    "object_attributes": {
-        "id": 382,
-        "title": "test2",
-        "assignee_id": 6,
-        "author_id": 6,
-        "project_id": 65,
-        "created_at": "2014-05-26T00:30:43.350Z",
-        "updated_at": "2014-05-26T00:30:43.350Z",
-        "position": 0,
-        "branch_name": null,
-        "description": "aaa",
-        "milestone_id": null,
-        "state": "opened",
-        "iid": 2
-    }
+  object_kind: 'issue',
+  user: {
+    name: 'shigeru.nakajima',
+    username: 'shigeru.nakajima',
+    avatar_url: 'https://secure.gravatar.com/avatar/3a1f432882d487dfbc0046fbdca602b6?s=40&d=identicon'
+  },
+  object_attributes: {
+    id: 2624,
+    title: 'a',
+    assignee_id: null,
+    author_id: 6,
+    project_id: 65,
+    created_at: '2015-07-21 10:35:22 UTC',
+    updated_at: '2015-07-21 10:36:16 UTC',
+    position: 0,
+    branch_name: null,
+    description: 'a\r\nb',
+    milestone_id: null,
+    state: 'opened',
+    iid: 4,
+    url: 'https://developer.luxiar.jp/shigeru.nakajima/chabot/issues/4',
+    action: 'open'
+  }
 };
 
 frisby.create('issue')
@@ -41,4 +49,16 @@ frisby.create('異なるチャットに送る場合は別のissueとして扱い
     })
     .expectStatus(200)
     .expectBodyContains('OK')
+    .toss();
+
+frisby.create('actionがupdateなissueは無視します。')
+    .post('http://localhost:3000/gitlab/123456', extend({}, issueData, {
+        object_kind:{
+            action:'update'
+        }
+    }), {
+        json: true
+    })
+    .expectStatus(200)
+    .expectBodyContains('Ignore')
     .toss();

5c4920adb76c5c5482c9ef4eaa980310e1df6c93
ledsun <shigeru.nakajima@gmail.com>
2015-08-25 20:30:27 +0900

    Change the getObjectKind to a middleware of the connect.

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import connect from 'connect'
 import bodyParser from 'body-parser'
 import connectRoute from 'connect-route'
-import gitlab from './lib/gitlab';
+import objectKindParser from './lib/objectKindParser'
+import gitlab from './lib/gitlab'
+
+let gitlabMiddleware = connect()
+  .use(objectKindParser)
+  .use(gitlab)
 
 connect()
   .use(bodyParser.json())
-  .use(connectRoute(router => {
-    router.post('/gitlab/:chatId', gitlab);
-  }))
+  .use(connectRoute(router => router.post('/gitlab/:chatId', gitlabMiddleware)))
   .listen(process.env.PORT || 3000);

b42d29e7fc13fa53add52619f203991de15d9b09
ledsun <shigeru.nakajima@gmail.com>
2015-09-04 19:51:38 +0900

    Rename a middleware.

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 import connect from 'connect'
 import bodyParser from 'body-parser'
 import connectRoute from 'connect-route'
-import objectKindParser from './lib/objectKindParser'
-import gitlab from './lib/gitlab'
 
-import checkIgnore from './lib/check-ignore'
+import objectKindParser from './lib/objectKindParser'
 import AvoidingDoubleTransmission from './lib/AvoidingDoubleTransmission'
+import suppressor from './lib/suppressor'
+import gitlab from './lib/gitlab'
 
 let gitlabMiddleware = connect()
   .use(objectKindParser)
   .use(new AvoidingDoubleTransmission())
-  .use(checkIgnore)
+  .use(suppressor)
   .use(gitlab)
 
 connect()

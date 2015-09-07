e0dcaa9aa74a9eb33807b7b6a15202431b1bb90c
ledsun <shigeru.nakajima@gmail.com>
2015-09-07 20:08:35 +0900

    Extract a module of the gitlabMiddleware.

--- app.js
+++ app.js
@@ -2,27 +2,7 @@ import connect from 'connect'
 import bodyParser from 'body-parser'
 import connectRoute from 'connect-route'
 
-import objectKindParser from './lib/objectKindParser'
-import AvoidingDoubleTransmission from './lib/AvoidingDoubleTransmission'
-import suppressor from './lib/suppressor'
-import debug from './lib/debug'
-import sendIgnore from './lib/sendIgnore'
-import gitlabInfo from './lib/gitlabInfo'
-import sendChatwork from './lib/sendChatwork'
-import format from './lib/format'
-import sendOK from './lib/sendOK'
-
-
-let gitlabMiddleware = connect()
-  .use(objectKindParser)
-  .use(new AvoidingDoubleTransmission())
-  .use(suppressor)
-  .use(debug)
-  .use(sendIgnore)
-  .use(gitlabInfo)
-  .use(format)
-  .use(sendChatwork)
-  .use(sendOK)
+import gitlabMiddleware from './lib/gitlabMiddleware'
 
 connect()
   .use(bodyParser.json())
